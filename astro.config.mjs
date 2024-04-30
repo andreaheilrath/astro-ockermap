ba1196d7d6f37967002995d826f6b6746c5fe862
Andrea Heilrath <49402693+andreaheilrath@users.noreply.github.com>
2024-04-30 14:47:26 +0200

    updating index to masonry grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [mdx()],
 
   site: `https://andreaheilrath.github.io/astro-ockermap/`,
-  base: '/astro-ockermap',
+  base: '/',
   output: 'static',
   outDir: './docs',
   build: {
